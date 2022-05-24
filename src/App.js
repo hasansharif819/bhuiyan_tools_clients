cf688bf32b65cc2a4f61b66f5a9c973450cf87c8
hasansharif819 <hs.sharif819@gmail.com>
2022-05-25 00:25:02 +0600

    Route setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home/Home';
+import Login from './pages/Login/Login/Login';
+import Portfolio from './pages/Portfolio/Portfolio';
+import Products from './pages/Products/Products/Products';
+import Purchase from './pages/Purchase/Purchase/Purchase';
 import Footer from './pages/Shared/Footer/Footer';
 import Navbar from './pages/Shared/Navbar/Navbar';
 
@@ -11,6 +15,10 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path='/purchase' element={<Purchase></Purchase>}></Route>
+        <Route path='/portfolio' element={<Portfolio></Portfolio>}></Route>
+        <Route path='login' element={<Login></Login>}></Route>
       </Routes>
 
 
